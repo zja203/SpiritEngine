01bab12b9f672b097c91a407a42e538d54cbda2d
Zack Amoroso <38058764+zja203@users.noreply.github.com>
2022-10-24 12:01:12 -0400

    Added asserts

--- spirit/include/core/base.hpp
+++ spirit/include/core/base.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #if defined(__linux__) || defined(__linux) || defined(linux)
 	#define SPRT_PLATFORM_LINUX
 #elif defined(_WIN64)
@@ -14,8 +16,15 @@
 	#endif
 #elif defined(SPRT_PLATFORM_LINUX)
 	#define SPIRIT_API
+	#include <signal.h>
+	#define SPRT_BREAKDEBUGGER raise(SIGTRAP)
 #else
 	#error Spirit only supports (64 bit) Windows and Linux!
+#endif
+
+#ifdef SPRT_ENABLE_ASSERTS
+       #define SPRT_ASSERT(x, ...) { if(!(x)) { SPRT_ERROR("Assertion Failed: {0}", __VA_ARGS__); SPRT_BREAKDEBUGGER(); } }
+       #define SPRT_CORE_ASSERT(x, ...) { if(!(x)) { SPRT_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); SPRT_BREAKDEBUGGER; } }
 #endif
 
 #define BIT(x) (1 <<x )
