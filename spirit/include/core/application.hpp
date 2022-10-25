7a4959c16ea329bb0a7e19284d4874807f016265
Zack Amoroso <38058764+zja203@users.noreply.github.com>
2022-10-24 21:55:55 -0400

    Added AppSpec

--- spirit/include/core/application.hpp
+++ spirit/include/core/application.hpp
@@ -3,9 +3,26 @@
 #include "core/base.hpp"
 #include "events/event.hpp"
 #include "events/app_event.hpp"
+#include "layer_stack.hpp"
 #include "window.hpp"
 
 namespace Spirit {
+	struct ApplicationCommandLineArgs {
+		int Count = 0;
+		char **Args = nullptr;
+
+		const char *operator[](int index) const {
+			SPRT_CORE_ASSERT(index < Count);
+			return Args[index];
+		}
+	}
+
+	struct AppSpec {
+		std::string Name = "Spirit Application";
+		std::string WorkingDir;
+		ApplicationCommandLineArgs CommandLineArgs;
+	};
+
 	class SPIRIT_API Application {
 		public:
 			Application();
@@ -14,10 +31,18 @@ namespace Spirit {
 			void Run();
 
 			void OnEvent(Event& e);
+			void pushLayer(Layer *layer);
+			void pushOverlay(Layer *layer);
+
+			static Application& Get() { return *s_Instance; }
+
+			const ApplicationSpecification& GetSpecification() const { return m_Specification; }
 		private:
+			ApplicationSpecification m_Specification;
 			bool OnWindowClose(WindowCloseEvent& e);
 			std::unique_ptr<Window> m_Window;
 			bool m_Running = true;
+			LayerStack m_LayerStack;
 	};
 
 
